1804f356436db4928c726c3e4c0cd59e61505ba6
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-11-18 17:46:17 +0800

    测试成功

--- WorkerService1/Program.cs
+++ WorkerService1/Program.cs
@@ -1,8 +1,10 @@
+using ServiceReference1;
 using WorkerService1;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddSingleton<TestDataCollectionSoap>(new TestDataCollectionSoapClient(TestDataCollectionSoapClient.EndpointConfiguration.TestDataCollectionSoap12));
         services.AddHostedService<Worker>();
     })
     .Build();
--- WorkerService1/Worker.cs
+++ WorkerService1/Worker.cs
@@ -1,19 +1,28 @@
+using ServiceReference1;
+
 namespace WorkerService1
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly TestDataCollectionSoap _client;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, TestDataCollectionSoap client)
         {
             _logger = logger;
+            _client = client;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                var helloWorldResponse = await _client.HelloWorldAsync(new HelloWorldRequest());
+                var getCraftInfoResponse = await _client.GetCraftInfoAsync(new GetCraftInfoRequest());
+                _logger.LogInformation("Worker running at: {time}, {serialize}", DateTimeOffset.Now,
+                    helloWorldResponse.Body.HelloWorldResult);
+                _logger.LogInformation("Worker running at: {time}, {serialize}", DateTimeOffset.Now,
+                    getCraftInfoResponse.Body.GetCraftInfoResult.Result);
                 await Task.Delay(1000, stoppingToken);
             }
         }
