2fb524968b6b2ce9abaebfef3879b5466b6801af
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-11-21 14:30:24 +0800

    自定义网址

--- ServiceReferenceTest/Program.cs
+++ ServiceReferenceTest/Program.cs
@@ -1,10 +1,18 @@
 using ServiceReferenceTest;
 using Sfc.TestDataCollection;
+using wlg.WlgManagementService;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddSingleton<TestDataCollectionSoap>(new TestDataCollectionSoapClient(TestDataCollectionSoapClient.EndpointConfiguration.TestDataCollectionSoap12));
+        services.AddSingleton<TestDataCollectionSoap>(
+            new TestDataCollectionSoapClient(
+                TestDataCollectionSoapClient.EndpointConfiguration.TestDataCollectionSoap12));
+        // services.AddSingleton<TestDataCollectionSoap>(
+        //     new TestDataCollectionSoapClient(
+        //         TestDataCollectionSoapClient.EndpointConfiguration.TestDataCollectionSoap12, "http://localhost:36871/WebService/TestDataCollection.asmx"));
+        services.AddSingleton<WlgManagement>(
+            new WlgManagementClient(WlgManagementClient.EndpointConfiguration.WlgManagementPort));
         services.AddHostedService<Worker>();
     })
     .Build();
--- ServiceReferenceTest/Worker.cs
+++ ServiceReferenceTest/Worker.cs
@@ -1,24 +1,31 @@
 using Sfc.TestDataCollection;
+using wlg.WlgManagementService;
 
 namespace ServiceReferenceTest
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly TestDataCollectionSoap _client;
+        private readonly TestDataCollectionSoap _testDataCollection;
+        private readonly WlgManagement _wlgManagement;
 
-        public Worker(ILogger<Worker> logger, TestDataCollectionSoap client)
+        public Worker(ILogger<Worker> logger, TestDataCollectionSoap testDataCollection, WlgManagement wlgManagement)
         {
             _logger = logger;
-            _client = client;
+            _testDataCollection = testDataCollection;
+            _wlgManagement = wlgManagement;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                var helloWorldResponse = await _client.HelloWorldAsync(new HelloWorldRequest());
-                var getCraftInfoResponse = await _client.GetCraftInfoAsync(new GetCraftInfoRequest());
+                await using var s = new TestDataCollectionSoapClient(
+                    TestDataCollectionSoapClient.EndpointConfiguration.TestDataCollectionSoap12,
+                    "http://localhost:36871/WebService/TestDataCollection.asmx");
+                var testDataCollectionSoap = s as TestDataCollectionSoap;
+                var helloWorldResponse = await testDataCollectionSoap.HelloWorldAsync(new HelloWorldRequest());
+                var getCraftInfoResponse = await _testDataCollection.GetCraftInfoAsync(new GetCraftInfoRequest());
                 _logger.LogInformation("Worker running at: {time}, {serialize}", DateTimeOffset.Now,
                     helloWorldResponse.Body.HelloWorldResult);
                 _logger.LogInformation("Worker running at: {time}, {serialize}", DateTimeOffset.Now,
